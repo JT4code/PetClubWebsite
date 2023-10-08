5123dfa1ab4a27d111ce34b4d158d3de518ec054
JT4code <98114430+JT4code@users.noreply.github.com>
2023-10-08 09:40:34 +0100

    added browse-items.php

--- src/items/browse-items.php
+++ src/items/browse-items.php
@@ -7,11 +7,11 @@
     $item_type = 'Bedding';
     $pet_type = 'Cats';
 
-    $mysqli = require __DIR__ . "./";
+    $mysqli = require __DIR__ . "/stock-items-database.php";
 
     $sql = sprintf("SELECT * FROM items
-    WHERE Pet_type = 'cats' AND Item_type = 'bedding'");
-    #$mysqli->real_escape_string($pet_type), $mysqli->real_escape_string($item_type));
+    WHERE Pet_type = '%s' AND Item_type = '%s'",
+    $mysqli->real_escape_string($pet_type), $mysqli->real_escape_string($item_type));
       
     $result = $mysqli->query($sql);
     echo $item_type;
